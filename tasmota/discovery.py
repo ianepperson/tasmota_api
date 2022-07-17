d524b9f157f3e2958cf9d2d506450548523abcf9
Ian Epperson <ian@epperson.com>
2022-07-17 13:14:19 -0700

    Add online status to devices

--- tasmota/discovery.py
+++ tasmota/discovery.py
@@ -6,9 +6,10 @@ log = logging.getLogger(__name__)
 
 
 class Device:
-    def __init__(self, sn, client):
+    def __init__(self, sn, client, telemetry):
         self.sn = sn
         self._mqtt_client = client
+        self._telemetry = telemetry
         self._config = {}
         self._sensors = {}
 
@@ -42,11 +43,25 @@ class Device:
     def topic(self):
         return self._config.get('t')
 
+    @property
+    def online_message(self):
+        return self._config.get('onln') or 'Online'
+
+    @property
+    def online(self):
+        topic = f'tele/{self.topic}/LWT'
+        message = self._telemetry.get(topic)
+        if message is None or self.online_message is None:
+            return None
+
+        return message == self.online_message
+
 
 class Discover:
     def __init__(self, mqtt_client):
         self.client = mqtt_client
         self.devices = {}
+        self._telemetry = {}
 
         self._other_on_connect = mqtt_client.on_connect
         self._other_on_message = mqtt_client.on_message
@@ -73,7 +88,7 @@ class Discover:
 
         device = self.devices.get(sn)
         if not device:
-            device = Device(sn, self.client)
+            device = Device(sn, self.client, self._telemetry)
             self.devices[sn] = device
             log.info(f'discovered new device "{sn}"')
             for fn in self._new_device_callbacks:
@@ -99,6 +114,9 @@ class Discover:
             log.debug(f'updated sensors for {sn}')
             device.sensors = sensors.get('sn', {})
 
+    def _telemetry_msg(self, msg):
+        self._telemetry[msg.topic] = msg.payload.decode()
+
     def _on_connect(self, *args, **kwargs):
         if self._other_on_connect:
             self._other_on_connect(*args, **kwargs)
@@ -107,6 +125,9 @@ class Discover:
         if msg.topic.startswith('tasmota/discovery/'):
             log.debug(f'received discovery message')
             self._discovery_msg(msg)
+        elif msg.topic.startswith('tele/'):
+            log.debug(f'received telemetry message')
+            self._telemetry_msg(msg)
 
         if self._other_on_message:
             self._other_on_message(client, userdata, msg)
